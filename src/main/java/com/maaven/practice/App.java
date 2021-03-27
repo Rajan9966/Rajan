9ad0977dc02b666c2c20d4cd96de7403c23e05fe
Rajan9966 <rajan.meghu@gmail.com>
2021-03-27 22:35:21 +0530

    new files

--- src/main/java/com/maaven/practice/App.java
+++ src/main/java/com/maaven/practice/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+		int i=0;
+		if(i>0){
+			 System.out.println( "Hello World! greater" );
+		}
         System.out.println( "Hello World!" );
     }
 }
